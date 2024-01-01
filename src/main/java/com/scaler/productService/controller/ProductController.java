655a001b03263f32038d390d18f4058135e7972e
sai ram <sairam20929@gmail.com>
2024-01-01 16:31:08 +0530

    added product dto

--- src/main/java/com/scaler/productService/controller/ProductController.java
+++ src/main/java/com/scaler/productService/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.scaler.productService.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.scaler.productService.dto.CreateProductRequestDTO;
 
 @RestController
 @RequestMapping("/products")
@@ -12,5 +17,16 @@ public class ProductController {
 	public String getAllProducts() {
 		return "All products";
 	}
+	
+	@PostMapping("/")
+    public String createProduct(@RequestBody CreateProductRequestDTO dto){
+
+        return "product created.. + "+ dto.getProductName() +" "+ dto.getCategory();
+    }
+
+    @GetMapping("/{productId}")
+    public String getProductById(@PathVariable("productId") Integer productId){
+        return "user is---"+productId;
+    }
 
 }
