77a462b12d31cddd05545f185b34a4662eac5cae
sai ram <sairam20929@gmail.com>
2024-04-04 21:40:14 +0530

    modified SearchRequestDto

--- src/main/java/com/scaler/productService/dto/SearchRequestDto.java
+++ src/main/java/com/scaler/productService/dto/SearchRequestDto.java
@@ -1,4 +1,14 @@
 package com.scaler.productService.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SearchRequestDto {
+    private String query;
+
+    private int pageSize;
+
+    private int pageNumber;
 }
